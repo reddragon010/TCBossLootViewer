e64b41c0cca6f4330ecb116a512edc88dfb94453
Michael Riedmann <reddragon010@gmx.net>
2011-06-27 02:26:30 +0200

    added hero_loot and some nice style

--- classes/creature.php
+++ classes/creature.php
@@ -4,9 +4,7 @@
 * 
 */
 class Creature extends Model
-{
-	private $loot_list;
-	
+{	
 	public static function find($entry){
 		$sql = "SELECT entry, difficulty_entry_1 as hero_entry, name from creature_template where entry =".$entry.";";
 		$result = Database::query($sql)->fetch();
@@ -15,19 +13,24 @@ class Creature extends Model
 		}
 	}
 	
-	public function get_loot(){
+	public function get_loot($entry=null){
+		if(is_null($entry)){
+			$entry = $this->entry;
+		}
+		$d_loot = $this->find_direct_loot($this->entry);
+		$r_loot = $this->find_ref_loot($this->entry);
+		return array_merge($d_loot, $r_loot);
+	}
+	
+	public function get_hero_loot(){
+		return $this->get_loot($this->hero_entry);
+	}
+	
+	private function find_ref_loot($entry){
 		$loot = array();
 		$referances = array();
-		
-		//direct loot
-		$sql = "SELECT creature_loot_template.item as entry, 0 as ref, ChanceOrQuestChance as drop_chance, name FROM creature_loot_template INNER JOIN item_template ON (creature_loot_template.item = item_template.entry)  WHERE creature_loot_template.entry = (SELECT lootid FROM creature_template WHERE entry = {$this->entry} AND mincountorref > 0);";
-		$d_items = Database::query($sql)->fetchAll(PDO::FETCH_CLASS, 'Item');
-		if(count($d_items) > 0){
-			$loot = array_merge($loot, $d_items);
-		}
-		
 		//referances
-		$sql = "SELECT ABS(mincountorref) as refid, ChanceOrQuestChance as drop_chance FROM `creature_loot_template` WHERE entry IN(SELECT `lootid` FROM `creature_template` WHERE entry = {$this->entry} AND mincountorref < 0);";
+		$sql = "SELECT ABS(mincountorref) as refid, ChanceOrQuestChance as drop_chance FROM `creature_loot_template` WHERE entry IN(SELECT `lootid` FROM `creature_template` WHERE entry = {$entry} AND mincountorref < 0);";
 		$referances = Database::query($sql)->fetchAll();
 
 		//referanced loot
@@ -40,8 +43,17 @@ class Creature extends Model
 			$items = $result->fetchAll(PDO::FETCH_CLASS, 'Item');
 			$loot = array_merge($loot, $items);
 		}
-		$this->loot_list = $loot;
 		return $loot;
+	}
+	
+	private function find_direct_loot($entry){
+		$d_items  = array();
+		$sql = "SELECT creature_loot_template.item as entry, 0 as ref, ChanceOrQuestChance as drop_chance, name FROM creature_loot_template INNER JOIN item_template ON (creature_loot_template.item = item_template.entry)  WHERE creature_loot_template.entry = (SELECT lootid FROM creature_template WHERE entry = {$entry} AND mincountorref > 0);";
+		$items = Database::query($sql)->fetchAll(PDO::FETCH_CLASS, 'Item');
+		if($items){
+			$d_items = $items;
+		}
+		return $d_items;
 	}
 	
 }
--- index.php
+++ index.php
@@ -9,6 +9,8 @@ $db = Database::start();
 if(isset($_GET['id'])){
 	$creature_id = (int)$_GET['id'];	
 	$creature = Creature::find($creature_id);
+	$loot = $creature->loot;
+	$hero_loot = $creature->hero_loot;
 }
 
 ?>
@@ -18,41 +20,82 @@ if(isset($_GET['id'])){
 	<html lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>untitled</title>
-		<meta name="generator" content="TextMate http://macromates.com/">
-		<meta name="author" content="Michael Riedmann">
-		<!-- Date: 2011-06-25 -->
+		<title>TC-LootViewer <?php echo $creature->name ?></title>
+		<link rel="stylesheet" href="style.css" type="text/css" charset="utf-8">
 	</head>
 	<body>
-		<div>
+		<table>
+			<tr>
+			<td>
 			<form action="index.php" method="get">
 				<lable for="id">ID</lable>
 				<input type="text" name="id" id="creature-id" <?php if(isset($creature_id)){echo 'value="'.$creature_id.'"';}?> />
 				<input type="submit" name="submit" />
 			</form>
-		</div>
+			</td>
+			</tr>
+		</table>
 		<?php if(isset($creature)){ ?>
-		<h1><?php echo $creature->name ?></h1>
 		<table>
+			<th><h1><?php echo $creature->name ?></h1></th>
+		</table>
+		
+		<div style="width:50%;float:left">
+		
+		<table>
+			<tr>
+				<th colspan="4"><h2>Normal Loot</h2></th>
+			</tr>
 			<tr>
 				<th>Entry</th>
 				<th>Name</th>
 				<th>Ref</th>
-				<th>DropChance</th>
+				<th>Drop</th>
 			</tr>
 			<?php
-		  foreach($creature->loot as $item){
+		  for($i=0;$i<count($loot);$i++){
+			$item = $loot[$i];
+			$class = ($i % 2 == 0) ? ' class="alt" ' : ''; 
 			?>
 			<tr>
-				<td><?php echo $item->entry ?></td>
-				<td><?php echo $item->name ?></td>
-				<td><?php echo $item->ref ?></td>
-				<td><?php echo $item->drop_chance ?></td>
+				<td<?php echo $class ?>><?php echo $item->entry ?></td>
+				<td<?php echo $class ?>><?php echo $item->name ?></td>
+				<td<?php echo $class ?>><?php echo $item->ref ?></td>
+				<td<?php echo $class ?>><?php echo $item->drop_chance ?></td>
 			</tr>
 			<?php
 			}
 			?>	
 		</table>
+		</div>
+		
+		<div style="width:50%; float:left">
+		<table>
+			<tr>
+				<th colspan="4"><h2>Hero Loot</h2></th>
+			</tr>
+			<tr>
+				<th>Entry</th>
+				<th>Name</th>
+				<th>Ref</th>
+				<th>Drop</th>
+			</tr>
+			<?php
+		  for($i=0;$i<count($hero_loot);$i++){
+			$item = $loot[$i];
+			$class = ($i % 2 == 0) ? ' class="alt" ' : '';
+			?>
+			<tr>
+				<td<?php echo $class ?>><?php echo $item->entry ?></td>
+				<td<?php echo $class ?>><?php echo $item->name ?></td>
+				<td<?php echo $class ?>><?php echo $item->ref ?></td>
+				<td<?php echo $class ?>><?php echo $item->drop_chance ?></td>
+			</tr>
+			<?php
+			}
+			?>	
+		</table>
+		</div>
 		<?php } ?>
 	</body>
 	</html>
